cb9685a6e4fcacf26bb115fa2a1d6dc334291437
veplaini <victor.lazzarini@nuim.ie>
2014-02-14 11:13:29 +0000

    changed path to url

--- csound.js
+++ csound.js
@@ -31,7 +31,7 @@ var csound = (function() {
     model.setAttribute('name', 'csound_module');
     model.setAttribute('id', 'csound_module');
     model.setAttribute('path', 'pnacl/Release');
-    model.setAttribute('src', 'pnacl/Release/csound.nmf');
+    model.setAttribute('src', 'http://vlazzarini.github.io/pnacl/Release/csound.nmf');
     var mimetype = 'application/x-pnacl';
     model.setAttribute('type', mimetype);
     var csoundhook = document.getElementById('engine');
