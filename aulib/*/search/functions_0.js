18b0a1354023ff5e9f3f6cbfabf5005284c99f9c
veplaini <victor.lazzarini@nuim.ie>
2017-01-15 20:01:41 +0000

    updated docs

---
+++ aulib/*/search/functions_0.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['adsr',['Adsr',['../class_au_lib_1_1_adsr.html#a591eebf9a8d640aca95e235336620509',1,'AuLib::Adsr']]],
+  ['allpass',['AllPass',['../class_au_lib_1_1_all_pass.html#a0b11b3a750afc969c7e80aac8fb56488',1,'AuLib::AllPass']]],
+  ['am_5ffm',['am_fm',['../class_au_lib_1_1_bl_osc.html#a2b7d75a7e9c63c72a8fb45aac02758a4',1,'AuLib::BlOsc::am_fm()'],['../class_au_lib_1_1_oscil.html#ae5281900edb947c70de366f79c9f4eca',1,'AuLib::Oscil::am_fm()'],['../class_au_lib_1_1_sample_player.html#a8d45c67e581189ee217ed36c92af3019',1,'AuLib::SamplePlayer::am_fm()']]],
+  ['audiobase',['AudioBase',['../class_au_lib_1_1_audio_base.html#aea9683208b42d11533ac506d7d03fb6a',1,'AuLib::AudioBase']]]
+];
--- aulib/search/functions_0.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['adsr',['Adsr',['../class_au_lib_1_1_adsr.html#a591eebf9a8d640aca95e235336620509',1,'AuLib::Adsr']]],
-  ['allpass',['AllPass',['../class_au_lib_1_1_all_pass.html#a0b11b3a750afc969c7e80aac8fb56488',1,'AuLib::AllPass']]],
-  ['am_5ffm',['am_fm',['../class_au_lib_1_1_oscil.html#a00f38a6f48259ee1be4f104a4ba0c5ee',1,'AuLib::Oscil']]],
-  ['audiobase',['AudioBase',['../class_au_lib_1_1_audio_base.html#aea9683208b42d11533ac506d7d03fb6a',1,'AuLib::AudioBase']]]
-];
