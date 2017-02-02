f79e1c9b5ae1666028912f9070015dd4c862b45c
veplaini <victor.lazzarini@nuim.ie>
2017-02-02 13:39:42 +0000

    update

--- cpof/search/all_d.js
+++ cpof/search/all_d.js
@@ -6,6 +6,7 @@ var searchData=
   ['plugin',['plugin',['../namespacecsnd.html#ad6914482b8d33e06c6d5ccf7d31d42ca',1,'csnd::plugin(CSOUND *csound, const char *name, const char *oargs, const char *iargs, uint32_t thread, uint32_t flags=0)'],['../namespacecsnd.html#a8a7df0de2c9a022c48e3eed5b6d4b31c',1,'csnd::plugin(CSOUND *csound, const char *name, uint32_t thread, uint32_t flags=0)']]],
   ['plugin',['Plugin',['../structcsnd_1_1_plugin.html',1,'csnd']]],
   ['plugin_2eh',['plugin.h',['../plugin_8h.html',1,'']]],
+  ['plugin_5fdeinit',['plugin_deinit',['../classcsnd_1_1_csound.html#a9d98c983b8f18f953c36484360006831',1,'csnd::Csound']]],
   ['polar',['polar',['../namespacecsnd.html#a392e9133842c8924ff5f7756dfaecd9da8032d9060aa9a695cae7da0c8612833b',1,'csnd']]],
   ['pvs',['pvs',['../namespacecsnd.html#a392e9133842c8924ff5f7756dfaecd9dab73f233f81430801803dabe8c7e079ac',1,'csnd']]],
   ['pvsbin',['pvsbin',['../classcsnd_1_1pvsbin.html#a0b2aba2fb337a3aa24ce5ee0a484ccf8',1,'csnd::pvsbin']]],
--- cpof/search/functions_d.js
+++ cpof/search/functions_d.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['perf_5ferror',['perf_error',['../classcsnd_1_1_csound.html#a4de81f3c78285ac584d2f01dd2be7f2d',1,'csnd::Csound']]],
   ['plugin',['plugin',['../namespacecsnd.html#ad6914482b8d33e06c6d5ccf7d31d42ca',1,'csnd::plugin(CSOUND *csound, const char *name, const char *oargs, const char *iargs, uint32_t thread, uint32_t flags=0)'],['../namespacecsnd.html#a8a7df0de2c9a022c48e3eed5b6d4b31c',1,'csnd::plugin(CSOUND *csound, const char *name, uint32_t thread, uint32_t flags=0)']]],
+  ['plugin_5fdeinit',['plugin_deinit',['../classcsnd_1_1_csound.html#a9d98c983b8f18f953c36484360006831',1,'csnd::Csound']]],
   ['pvsbin',['pvsbin',['../classcsnd_1_1pvsbin.html#a0b2aba2fb337a3aa24ce5ee0a484ccf8',1,'csnd::pvsbin']]]
 ];
