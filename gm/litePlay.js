8efcde6fedb52a9cacffc1bfe65b9e99e91f041d
vlazzarini <victor.lazzarini@mu.ie>
2022-12-24 12:25:06 +0000

    improving code

--- gm/litePlay.js
+++ gm/litePlay.js
@@ -1,6 +1,6 @@
 // csound.js is the Csound WASM module
 const csoundjs =
-  "https://cdn.jsdelivr.net/npm/@csound/browser@6.18.5/dist/csound.js";
+      "https://cdn.jsdelivr.net/npm/@csound/browser@6.18.5/dist/csound.js";
 // csound is the Csound engine object (null as we start)
 let csound = null;
 // audio context
@@ -13,296 +13,290 @@ const sfont = "./gm.sf2";
 
 // this is the JS function to start Csound
 export async function startEngine(func = null) {
-  // if the Csound object is not initialised
-  if (csound == null) {
-    // import the Csound method from csound.js
-    const { Csound } = await import(csoundjs);
-    // create a Csound engine object
-    csound = await Csound();
-    // get audio context
-    audio_context = await csound.getAudioContext();
-    // set realtime audio (dac) output
-    await csound.setOption("-odac");
-    // set realtime MIDI input
-    await csound.setOption("-M0");
-    // copy the sfont file to the Csound local filesystem
-    await copyUrlToLocal(srcurl + sfont, sfont);
-    // copy the CSD file to the Csound local filesystem
-    await copyUrlToLocal(srcurl + csd, csd);
-    // compile csound code
-    await csound.compileCsd(csd);
-    // start the engine
-    await csound.start();
-  }
-  if(func) func();
+    // if the Csound object is not initialised
+    if (csound == null) {
+        // import the Csound method from csound.js
+        const { Csound } = await import(csoundjs);
+        // create a Csound engine object
+        csound = await Csound();
+        // get audio context
+        audio_context = await csound.getAudioContext();
+        // set realtime audio (dac) output
+        await csound.setOption("-odac");
+        // set realtime MIDI input
+        await csound.setOption("-M0");
+        // copy the sfont file to the Csound local filesystem
+        await copyUrlToLocal(srcurl + sfont, sfont);
+        // copy the CSD file to the Csound local filesystem
+        await copyUrlToLocal(srcurl + csd, csd);
+        // compile csound code
+        await csound.compileCsd(csd);
+        // start the engine
+        await csound.start();
+    }
+    if(func) func();
 }
 
 // copy URL to local file
 async function copyUrlToLocal(src, dest) {
-  // fetch the file
-  let srcfile = await fetch(src, { cache: "no-store" });
-  // get the file data as an array
-  let dat = await srcfile.arrayBuffer();
-  // write the data as a new file in the filesystem
-  await csound.fs.writeFile(dest, new Uint8Array(dat));
+    // fetch the file
+    const srcfile = await fetch(src, { cache: "no-store" });
+    // get the file data as an array
+    const dat = await srcfile.arrayBuffer();
+    // write the data as a new file in the filesystem
+    await csound.fs.writeFile(dest, new Uint8Array(dat));
 }
 
 export function midi(stat, b1, b2) {
-  csound.midiMessage(stat, b1, b2);
+    csound.midiMessage(stat, b1, b2);
 }
 
 export function midiProgram(n, chn = 1) {
-  if (chn >= 1 && chn <= 16) csound.midiMessage(chn + 191, n, 0);
+    if (chn >= 1 && chn <= 16) csound.midiMessage(chn + 191, n, 0);
 }
 
 const globalObj = {
- freeChannel: 16,
- BPM: 60.0
+    freeChannel: 16,
+    BPM: 60.0
 };
 
 class Instrument {
-  constructor(pgm, isDrums = false, rel = 0.1, instr = 10) {
-    this.pgm = pgm;
-    this.chn = globalObj.freeChannel++;
-    this.rel = rel;
-    this.isDrums = isDrums;
-    this.what = 60;
-    this.howLoud = 1;
-    this.howLong = 1;
-    this.on = new Uint8Array(128);
-    this.instr = 10
-  }
-
-  score(what, howLoud, when, howLong) {
-    let rel = this.rel;
-    let prog = this.pgm;
-    let instr = this.instr + what / 1000000 + this.chn / 1000;
-    if (this.isDrums) {
-      if (prog == 7) rel = 2;
-      else rel = 0.5;
-      if (what == 29 || what == 30) instr = 10.97;
-      else if (what == 42 || what == 44 || what == 46 || what == 49)
-        instr = 10.91;
-      else if (what == 71 || what == 72) instr = 10.92;
-      else if (what == 73 || what == 74) instr = 10.93;
-      else if (what == 78 || what == 79) instr = 10.94;
-      else if (what == 80 || what == 81) instr = 10.95;
-      else if (what == 86 || what == 87) instr = 10.96;
-      prog = 317 + this.pgm;
+    constructor(pgm, isDrums = false, rel = 0.1, instr = 10) {
+        this.pgm = pgm;
+        this.chn = globalObj.freeChannel++;
+        this.rel = rel;
+        this.isDrums = isDrums;
+        this.what = 60;
+        this.howLoud = 1;
+        this.howLong = 1;
+        this.on = new Uint8Array(128);
+        this.instr = instr;
     }
 
-    if (howLong <= 0) this.on[what] = 1;
-    if (howLoud < 0) {
-      instr *= -1;
-      this.on[what] = 0;
-    }
-
-    return (
-      "i" +
-      instr +
-      " " +
-      secs(when) +
-      " " +
-      (howLong > 0 ? secs(howLong) : -1) +
-      " " +
-      what +
-      " " +
-      127 * howLoud +
-      " " +
-      prog +
-      " " +
-      " " +
-      this.chn +
-      " " +
-      rel +
-      "\n"
-    );
-  }
-
-  event(what, howLoud = this.howLoud, when = 0, howLong = this.howLong) {
-    return [what, howLoud, when, howLong, this];
-  }
-
-  play(what = this.what, howLoud = this.howLoud, when = 0, howLong = 0) {
-    csound.inputMessage(this.score(what, howLoud, when, howLong));
-    return what;
-  }
-
-  stop(what = -1, when = 0) {
-    if (what < 0) {
-      for (what = 0; what < 128; what++) {
-        if (this.on[what]) {
-          csound.inputMessage(this.score(what, 0, when, 0));
+    score(what, howLoud, when, howLong) {
+        let rel = this.rel;
+        let prog = this.pgm;
+        let instr = this.instr + what / 1000000 + this.chn / 1000;
+        if (this.isDrums) {
+            if (prog == 7) rel = 2;
+            else rel = 0.5;
+            if (what == 29 || what == 30) instr = 10.97;
+            else if (what == 42 || what == 44 || what == 46 || what == 49)
+                instr = 10.91;
+            else if (what == 71 || what == 72) instr = 10.92;
+            else if (what == 73 || what == 74) instr = 10.93;
+            else if (what == 78 || what == 79) instr = 10.94;
+            else if (what == 80 || what == 81) instr = 10.95;
+            else if (what == 86 || what == 87) instr = 10.96;
+            prog = 317 + this.pgm;
         }
-      }
-    } else csound.inputMessage(this.score(what, 0, when, 0));
-    return what;
-  }
+
+        if (howLong <= 0) this.on[what] = 1;
+        if (howLoud < 0) {
+            instr *= -1;
+            this.on[what] = 0;
+        }
+
+        return (
+            "i" +
+                instr +
+                " " +
+                secs(when) +
+                " " +
+                (howLong > 0 ? secs(howLong) : -1) +
+                " " +
+                what +
+                " " +
+                127 * howLoud +
+                " " +
+                prog +
+                " " +
+                " " +
+                this.chn +
+                " " +
+                rel +
+                "\n"
+        );
+    }
+
+    event(what, howLoud = this.howLoud, when = 0, howLong = this.howLong) {
+        return [what, howLoud, when, howLong, this];
+    }
+
+    play(what = this.what, howLoud = this.howLoud, when = 0, howLong = 0) {
+        csound.inputMessage(this.score(what, howLoud, when, howLong));
+        return what;
+    }
+
+    stop(what = -1, when = 0) {
+        if (what < 0) {
+            for (what = 0; what < 128; what++) {
+                if (this.on[what]) {
+                    csound.inputMessage(this.score(what, 0, when, 0));
+                }
+            }
+        } else csound.inputMessage(this.score(what, 0, when, 0));
+        return what;
+    }
 }
 
 const MSEC = 1000;
 export function secs(b) {
-  return (b * 60.0) / globalObj.BPM;
+    return (b * 60.0) / globalObj.BPM;
 }
 
 export function beats(s) {
-  return (s * globalObj.BPM) / 60.0;
+    return (s * globalObj.BPM) / 60.0;
 }
 
 export function setBpm(bpm) {
-  globalObj.BPM = bpm;
+    globalObj.BPM = bpm;
 }
 
 export function getBpm() {
-  return globalObj.BPM; 
+    return globalObj.BPM; 
 }
 
 export function audioClock() {
-  if (audio_context) return audio_context.currentTime;
-  else return 0;
+    if (audio_context) return audio_context.currentTime;
+    else return 0;
 }
 
 export const sequencer = {
-  clickOn: false,
-  seqList: [],
-  time: 0.0,
-  callback: null,
-  click: function (ref) {
-    if (!this.clickOn) return;
-    let t = secs(1);
-    let delta = audioClock() - (t + ref);
-    if (delta >= 0) {
-      this.time = t + audioClock() - delta;
-      this.seqList.forEach((v, i, a) => {
-        v.play(beats(t - delta));
-      });
-      if (this.callback) {
-        let s = this.callback;
-        this.callback = null;
-        s(beats(t - delta));
-      }
-      setTimeout(this.click.bind(this, audioClock() - delta));
-    } else setTimeout(this.click.bind(this, ref));
-  },
-  sequence: function (i, w, a, b) {
-    return {
-      instr: i,
-      what: w,
-      amp: a,
-      bbs: b,
-      n: 0,
-      on: true,
-      play: function (sched) {
-        let what = this.what,
-          bbs = this.bbs,
-          amp = this.amp,
-          theInstr = this.instr,
-          dur = theInstr.isDrums ? 0 : this.bbs,
-          snd,
-          evt;
-        for (let i = 0; i < 1 / bbs; i++) {
-          evt = what[this.n];
-          this.n = this.n != what.length - 1 ? this.n + 1 : 0;
-          if (typeof evt === "object") {
-            snd = evt[0];
-            if (evt.length > 1) amp = evt[1];
-            if (evt.length > 2) sched += evt[2];
-            if (evt.length > 3) dur = evt[3];
-            if (evt.length > 4) {
-              theInstr = evt[4];
-              dur = dur > 0 ? dur : theInstr.isDrums ? 0 : t;
+    clickOn: false,
+    seqList: [],
+    time: 0.0,
+    callback: null,
+    click: function (ref) {
+        if (!this.clickOn) return;
+        let t = secs(1);
+        let delta = audioClock() - (t + ref);
+        if (delta >= 0) {
+            this.time = t + audioClock() - delta;
+            this.seqList.forEach((v, i, a) => {
+                v.play(beats(t - delta));
+            });
+            if (this.callback) {
+                let s = this.callback;
+                this.callback = null;
+                s(beats(t - delta));
             }
-          } else snd = evt;
-          if (sched >= 0 && snd >= 0 && this.on)
-            theInstr.play(snd, amp, sched + i * bbs, dur);
+            setTimeout(this.click.bind(this, audioClock() - delta));
+        } else setTimeout(this.click.bind(this, ref));
+    },
+    sequence: function (i, w, a, b) {
+        return {
+            instr: i,
+            what: w,
+            amp: a,
+            bbs: b,
+            n: 0,
+            on: true,
+            play: function (sched) {
+                const what = this.what, bbs = this.bbs;
+                let amp = this.amp,theInstr = this.instr,
+                    dur = theInstr.isDrums ? 0 : this.bbs;
+                for (let i = 0; i < 1 / bbs; i++) {
+                    let evt = what[this.n], snd = evt;
+                    this.n = this.n != what.length - 1 ? this.n + 1 : 0;
+                    if (typeof evt === "object") {
+                        snd = evt[0];
+                        if (evt.length > 1) amp = evt[1];
+                        if (evt.length > 2) sched += evt[2];
+                        if (evt.length > 3) dur = evt[3];
+                        if (evt.length > 4) {
+                            theInstr = evt[4];
+                            dur = dur > 0 ? dur : theInstr.isDrums ? 0 : t;
+                        }
+                    } 
+                    if (sched >= 0 && snd >= 0 && this.on)
+                        theInstr.play(snd, amp, sched + i * bbs, dur);
+                }
+            },
+        };
+    },
+    play: function (instr, what, howLoud, bbs = 1) {
+        let id = this.seqList.length;
+        if (bbs > 1) bbs = 1;
+        this.seqList.push(this.sequence(instr, what, howLoud, bbs));
+        return id;
+    },
+    start: function () {
+        if(!this.clickOn) {
+            this.clickOn = true;
+            this.click(audioClock());
         }
-      },
-    };
-  },
-  play: function (instr, what, howLoud, bbs = 1) {
-    let id = this.seqList.length;
-    if (bbs > 1) bbs = 1;
-    this.seqList.push(this.sequence(instr, what, howLoud, bbs));
-    return id;
-  },
-  start: function () {
-    if(!this.clickOn) {
-     this.clickOn = true;
-     this.click(audioClock());
-    }
-  },
-  stop: function () {
-    this.clickOn = false;
-    this.seqList.forEach( (v,i,a) => { v.n = 0 });
-  },
-  clear: function () {
-    this.seqList = [];
-  },
-  togglePause () {
-    this.clickOn =! this.clickOn;
-    this.click(audioClock());
-  },
-  toggleMute: function (id = -1) {
-    if (id != -1) this.seqList[id].on = !this.seqList[id].on;
-    else
-      this.seqList.forEach((v, i, a) => {
-        v.on = !v.on;
-      });
-  },
-  toggleSolo: function (id) {
-    this.seqList.forEach((v, i, a) => {
-      if (i != id) v.on = !v.on;
-    });
-  },
-  clock: function () {
-    return this.time;
-  },
-  isRunning: function () {
-    return this.clickOn;
-  },
+    },
+    stop: function () {
+        this.clickOn = false;
+        this.seqList.forEach( (v,i,a) => { v.n = 0 });
+    },
+    clear: function () {
+        this.seqList = [];
+    },
+    togglePause () {
+        this.clickOn =! this.clickOn;
+        this.click(audioClock());
+    },
+    toggleMute: function (id = -1) {
+        if (id != -1) this.seqList[id].on = !this.seqList[id].on;
+        else
+            this.seqList.forEach((v, i, a) => {
+                v.on = !v.on;
+            });
+    },
+    toggleSolo: function (id) {
+        this.seqList.forEach((v, i, a) => {
+            if (i != id) v.on = !v.on;
+        });
+    },
+    clock: function () {
+        return this.time;
+    },
+    isRunning: function () {
+        return this.clickOn;
+    },
 };
 
 export const eventList = {
-  events: [],
-  play: function (when = 0, evtLst = this.events) {
-    this.score(when, evtLst).play();
-    this.events = evtLst;
-  },
-  create: function (evtLst = []) {
-    let e = Object.create(eventList);
-    e.events = evtLst;
-    return e;
-  },
-  score: function (when = 0, evtLst = this.events) {
-    let mess = "";
-    let instr = piano,
-      amp = piano.howLoud,
-      what,
-      dur = piano.howLong,
-      t = when;
-    for (let i = 0; i < evtLst.length; i++) {
-      let evt = evtLst[i];
-      if (typeof evt === "object") {
-        what = evt[0];
-        instr = evt.length > 4 ? evt[4] : instr;
-        dur = evt.length > 3 ? evt[3] : dur;
-        t = evt.length > 2 ? evt[2] : t + dur;
-        amp = evt.length > 1 ? evt[1] : amp;
-      } else {
-        what = evt;
-        amp = instr.howLoud;
-        t += dur;
-        dur = instr.howLong;
-      }
-      mess += instr.score(what, amp, t, dur);
-    }
-    return {
-      score: mess,
-      play: function () {
-        csound.inputMessage(this.score);
-      },
-    };
-  },
+    events: [],
+    play: function (when = 0, evtLst = this.events) {
+        this.score(when, evtLst).play();
+        this.events = evtLst;
+    },
+    create: function (evtLst = []) {
+        let e = Object.create(eventList);
+        e.events = evtLst;
+        return e;
+    },
+    score: function (when = 0, evtLst = this.events) {
+        let mess = "";
+        let instr = piano,
+            amp = piano.howLoud,
+            dur = piano.howLong,
+            t = when, what;
+        for (const evt of evtLst) {
+            if (typeof evt === "object") {
+                what = evt[0];
+                instr = evt.length > 4 ? evt[4] : instr;
+                dur = evt.length > 3 ? evt[3] : dur;
+                t = evt.length > 2 ? evt[2] : t + dur;
+                amp = evt.length > 1 ? evt[1] : amp;
+            } else {
+                what = evt;
+                amp = instr.howLoud;
+                t += dur;
+                dur = instr.howLong;
+            }
+            mess += instr.score(what, amp, t, dur);
+        }
+        return {
+            score: mess,
+            play: function () {
+                csound.inputMessage(this.score);
+            },
+        };
+    },
 };
 
 export const grandPiano = new Instrument(0);
